9596dede8280dab6d8cfe2b631953a3dd245db58
Florian Busch <flbusch@gmx.de>
2022-06-15 10:48:45 +0200

    fixed tweet schedule, moved triggers to .env

--- src/index.js
+++ src/index.js
@@ -97,7 +97,7 @@ const sendTweet = async category => {
 //  tweets and get blocked users//*/
 
 //get blocked users once at start and then once an hour
-// getBlockedUsers();
+getBlockedUsers();
 cron.schedule("* 30 * * * *", function() {
   getBlockedUsers()
 });
@@ -107,12 +107,12 @@ cron.schedule("0 00 10 * * *", function() {
 });
 
 //Muswiesentweet every day at 15.33
-cron.schedule("0 33 10 * * *", function() {
+cron.schedule("0 33 10 * * 2,4,6", function() {
   sendTweet('Muswiese');
 });
 
 //Vokabel-Tweet every wednesday, friday and sunday at 4.12 pm
-cron.schedule("0 12 16 * * 3,5,7", function() {
+cron.schedule("0 12 16 * * 3,5", function() {
   sendTweet('Vokabel');
 });
 
@@ -125,6 +125,10 @@ cron.schedule("0 30 14 * * 1,4", function() {
 cron.schedule("0 30 9 * * *", function() {
   sendDailyMail()
 });
+
+const loggBlock = () => console.log(blocks)
+setInterval(loggBlock, 5000)
+
 
 //exports for testing
 module.exports = { checkForBlockedWords };
